7a0e36e5fb6ab6d7689d6e9db403343fba6aeca8
Maya Kenigsberg <maya.kenigsberg@post.runi.ac.il>
2024-01-10 19:32:52 +0200

    first try

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,96 @@
 public class ArrayOps {
-    public static void main(String[] args) {
+    public static void main(String[] args){ 
+
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+       int missingint = 0;
+        int[] fullarray = new int[array.length+1];
+        for (int i=0; i<fullarray.length;i++)
+            fullarray[i]=i;
+        for (int i=0; i<fullarray.length;i++)
+        {
+            if(contains(array, fullarray[i])== false)
+            {
+                missingint=fullarray[i];
+            }
+        }
+        return missingint;
+        
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int[] array2= new int[array.length-1];
+        int maxval = -998;
+        int secondmax=-999;
+        for (int i=0; i<array.length;i++)
+        {
+            if(maxval<array[i])
+            maxval=array[i];
+        }
+        int j=0;
+        for (int i=0; i<array.length;i++)
+        {
+            if(array[i]!=maxval)
+            {
+                array2[j]=array[j];
+                j++;
+            }
+        }
+        for (int i=0; i<array2.length;i++)
+        {
+            if(secondmax<array2[i])
+            secondmax=array2[i];
+        }
+
+        return secondmax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int [] smallarray = array1.length < array1.length ? array1 : array2;
+        int [] bigarry = array1.length < array2.length ? array2 : array1;
+        for (int i=0;i<bigarry.length;i++)
+        {
+            if (contains(smallarray, bigarry[i])==false)
+            {
+                return false;
+            }
+        }
+
+         return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean checkincrease = true;
+        boolean checkdecrease = true;
+        for(int i=0;i<array.length-1;i++)// checks increase
+        {
+            if (array[i+1]<array[i])
+            checkincrease = false;
+        }
+        if (array[array.length-1]>array[array.length])
+            checkincrease=false;
+        //
+        for(int i=0;i<array.length-1;i++)// checks decrease
+        {
+            if (array[i+1]>array[i])
+            checkdecrease = false;
+        }
+        if (array[array.length-1]<array[array.length])
+            checkdecrease=false;
+
+        return (checkdecrease|| checkincrease);
     }
+    public static boolean contains(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++)
+        {
+        if (arr[i] == value) 
+            {
+            return true;
+            }
+        }
+        return false ;
+        }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -25,18 +25,149 @@ public class StringOps {
         
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest (String string)
+     {
+        String newstr = "";
+        String vowels="aeiou";
+        for(int i =0; i<string.length();i++)
+        {
+                 if (vowels.indexOf(string.charAt(i))!=-1)// is a vowel
+            {
+            newstr= newstr+upperCase(string.charAt(i));
+            }
+            else
+            {
+                if(string.charAt(i)==32)
+                    newstr= newstr+string.charAt(i);
+                else
+                newstr= newstr+lowerCase(string.charAt(i));
+            }
+        }
+
+        return newstr;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+       String answer ="";
+       int counter = 0;
+       while(string.charAt(counter)==32 && counter<string.length())
+       {
+        counter++;
+       }
+       while(string.charAt(counter)!=32 && counter<string.length())
+       {
+        answer=answer+lowerCase(string.charAt(counter));
+        counter++;
+       }
+       for (int i=counter; i<string.length();i++)
+       {
+        answer=answer+upperCase(string.charAt(i));
+        i++;
+       while(string.charAt(i)==32&& counter<string.length())
+        {
+            answer=answer+lowerCase(string.charAt(i));
+            i++;
+        }
+        while(string.charAt(i)==32 && counter<string.length())
+       {
+         i++;
+       }
+
+    }
+        return answer;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int[] array = new int[string.length()];
+        String draft=string;
+        int counter=0;
+        for(int i=0;i<string.length();i++)
+        {
+            if(string.charAt(i)==chr)
+            {
+                array[counter]=draft.indexOf(chr);
+                counter++;
+                draft="";
+                for (int j=0;j<draft.length();j++)
+                {
+                    if (i==j)
+                    {
+                     draft=draft+(string.charAt(i)+1);
+                    }
+                    else
+                    {
+                    draft=draft+string.charAt(i);
+                    }
+                }
+
+            }
+        }
+        int[] answer = new int[counter];
+        for(int i=0;i<answer.length;i++)
+        {
+            answer[i]=array[i];
+        }
+
+        return answer;
     }
+    public static String lowerCase(String s) 
+    {
+        String answer = "";
+        for (int i=0; i<s.length();i++)
+        {
+          char c = s.charAt(i);
+          if (c>=65 && c<=90)
+          {
+                c=(char)(c+32); 
+          }
+            answer=answer+c;
+        }
+        return answer;
+    }
+    public static char lowerCase(char s) 
+    {
+        char c=s;
+          if (c>=65 && c<=90)
+          {
+                c=(char)(c+32); 
+          }        
+        return c;
+    }
+    
+public static String upperCase(String s)
+ {
+        String answer = "";
+        for (int i=0; i<s.length();i++)
+        {
+          char c = s.charAt(i);
+          if (c>=97 && c<=122)
+          {
+                c=(char)(c-32); 
+          }
+            answer=answer+c;
+        }
+        return answer;
+    }
+    public static char upperCase(char s)
+ { 
+          char c = s;
+          if (c>=97 && c<=122)
+          {
+                c=(char)(c-32); 
+          }
+        return c;
+    }
+    
+    public static String uniqueChars(String s) {
+        String answer="";
+        for (int i=0; i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if( c == 32|| answer.indexOf(c)==-1)
+                answer=answer+c;
+        }
+        return answer;
+    }
+    
 }
+
